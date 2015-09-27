8adb8359ba4cb00ebbf2c379e822e7982959bd63
Mbond65 <mpbond@live.co.uk>
2015-09-28 00:02:09 +0100

    Uncomment during tests

--- TOR_transparentproxy.sh
+++ TOR_transparentproxy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#apt-get update
-#apt-get upgrade -y
+apt-get update
+apt-get upgrade -y
 
 echo "Does this system have a static IP [Y/N]?"
 read STATICIPSET;
@@ -16,21 +16,21 @@ if [ "$STATICIPSET" =  "N" ]; then
         exit
 fi
 
-#apt-get install tor -y
+apt-get install tor -y
 
 echo "TOR has been installed"
 echo "Please enter the IP address for this host"
 
 read STATICIP;
 
-#echo Log notice file /var/log/tor/notices.log >> /etc/tor/torrc
-#echo VirtualAddrNetwork 10.192.0.0/10 >> /etc/tor/torrc
-#echo AutomapHostsSuffixes .onion,.exit >> /etc/tor/torrc
-#echo AutomapHostsOnResolve 1 >> /etc/tor/torrc
-#echo TransPort 9040 >> /etc/tor/torrc
-#echo TransListenAddress $STATICIP >> /etc/tor/torrc
-#echo DNSPort 53 >> /etc/tor/torrc
-#echo DNSListenAddress $STATICIP >> /etc/tor/torrc
+echo Log notice file /var/log/tor/notices.log >> /etc/tor/torrc
+echo VirtualAddrNetwork 10.192.0.0/10 >> /etc/tor/torrc
+echo AutomapHostsSuffixes .onion,.exit >> /etc/tor/torrc
+echo AutomapHostsOnResolve 1 >> /etc/tor/torrc
+echo TransPort 9040 >> /etc/tor/torrc
+echo TransListenAddress $STATICIP >> /etc/tor/torrc
+echo DNSPort 53 >> /etc/tor/torrc
+echo DNSListenAddress $STATICIP >> /etc/tor/torrc
 
 /etc/init.d/tor start
 update-rc.d tor enable
